8e7582d6f3fff92429090f561c08f28fcaf4fe49
GregoireLgt <gregoire.laget@reseau.eseo.fr>
2020-12-20 19:32:03 +0100

    Add top navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: 'Resume',
+        url: '#', // put link there
+        cName: 'nav-links'
+    },
+    {
+        title: 'Projects',
+        url: '#', // put link there
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact me',
+        url: '#', // put link there
+        cName: 'nav-links'
+    },
+]
