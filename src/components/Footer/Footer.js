87ceac5e0d4fbd1888671e0d09057c10e01f85a3
GregoireLgt <gregoire.laget@reseau.eseo.fr>
2020-12-20 19:47:43 +0100

    Add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './Footer.css'
+
+
+const Footer = () => (
+    <div className="Footer">
+        <p>Grégoire Laget - All Rights Reserved &copy;</p>
+    </div>
+);
+
+export default Footer;
